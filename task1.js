3d01bb0a742fd162f7494b50f87895d0bc30b628
BNewing <bnewing92@hotmail.co.uk>
2016-11-16 18:11:41 +0000

    Form box now clears

--- task1.js
+++ task1.js
@@ -1,6 +1,7 @@
 var name;
 
 function getName() {
-  var name = document.getElementById("name").value
+  var name = document.getElementById("name").value;
   document.getElementById("yourName").innerHTML = name;
+  document.getElementById("form").reset();
 }
