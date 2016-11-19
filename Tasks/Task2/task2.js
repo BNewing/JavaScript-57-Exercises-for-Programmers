9f0df982860a9105f1de0c094acc40557705a621
BNewing <bnewing92@hotmail.co.uk>
2016-11-19 20:49:06 +0000

    Task 2 improvement

--- Tasks/Task2/task2.js
+++ Tasks/Task2/task2.js
@@ -1,5 +1,5 @@
 function getLength() {
   var length = document.getElementById("string").value.length;
-  document.getElementById("length").innerHTML = length;
+  document.getElementById("length").innerHTML = "The length of that string is: " + length;
   form.reset();
 }
