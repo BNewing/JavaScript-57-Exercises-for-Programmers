367dcfdb227737a1a1a3f448056cc16376fc227b
BNewing <bnewing92@hotmail.co.uk>
2016-11-23 20:18:09 +0000

    Functions Working

--- Tasks/Task5/task5.js
+++ Tasks/Task5/task5.js
@@ -1,10 +1,12 @@
 function maths() {
-  var firstNumber = document.getElementById("first").value;
-  var secondNumber = document.getElementById("second").value;
+  var firstNumberValue = document.getElementById("first").value;
+  var firstNumber = parseInt(firstNumberValue);
+  var secondNumberValue = document.getElementById("second").value;
+  var secondNumber = parseInt(secondNumberValue);
   var addition = firstNumber + secondNumber;
   var subtraction = firstNumber - secondNumber;
   var division = firstNumber/secondNumber;
-  var multiplication = firstNumber + secondNumber;
+  var multiplication = firstNumber * secondNumber;
   document.getElementById("add").innerHTML = firstNumber + " + " + secondNumber +  " = " + addition;
   document.getElementById("subtract").innerHTML = firstNumber + " - " + secondNumber +  " = " + subtraction;
   document.getElementById("divide").innerHTML = firstNumber + " / " + secondNumber +  " = " + division;
