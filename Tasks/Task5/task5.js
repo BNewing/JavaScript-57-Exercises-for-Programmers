bf76f2b89ac069f8792bb9f9eee3059699aae41d
BNewing <bnewing92@hotmail.co.uk>
2016-11-23 20:00:43 +0000

    All Functions printing on page

--- Tasks/Task5/task5.js
+++ Tasks/Task5/task5.js
@@ -2,5 +2,14 @@ function maths() {
   var firstNumber = document.getElementById("first").value;
   var secondNumber = document.getElementById("second").value;
   var addition = firstNumber + secondNumber;
+  var subtraction = firstNumber - secondNumber;
+  var division = firstNumber/secondNumber;
+  var multiplication = firstNumber + secondNumber;
   document.getElementById("add").innerHTML = firstNumber + " + " + secondNumber +  " = " + addition;
+  document.getElementById("subtract").innerHTML = firstNumber + " - " + secondNumber +  " = " + subtraction;
+  document.getElementById("divide").innerHTML = firstNumber + " / " + secondNumber +  " = " + division;
+  document.getElementById("multiply").innerHTML = firstNumber + " x " + secondNumber +  " = " + multiplication;
+
+
+
 }
