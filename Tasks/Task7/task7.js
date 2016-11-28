4c3c6c9fa08d1c2104f4d660a7e511c32d944f19
BNewing <bnewing92@hotmail.co.uk>
2016-11-28 23:52:49 +0000

    Did Challenge 1 for Task 7

--- Tasks/Task7/task7.js
+++ Tasks/Task7/task7.js
@@ -1,6 +1,16 @@
-function calculateArea() {
+function checkIfNumber() {
   var length = document.getElementById("length").value;
   var width = document.getElementById("width").value;
+  if (isNaN(length) != true && isNaN(width) != true){
+    calculateArea(width, length);
+  }
+  else {
+    document.getElementById("areaFeet").innerHTML = "You need to add in numbers!"
+    document.getElementById("areaMetres").innerHTML = "";
+  }
+}
+
+function calculateArea(width, length){
   var areaFeet = width * length;
   var metreConversionFactor = 0.09290304;
   var areaMetres = areaFeet * metreConversionFactor;
