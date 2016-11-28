f97bea4f882e5ec8c5d6eae5ecd01ba9c73b67a5
BNewing <bnewing92@hotmail.co.uk>
2016-11-28 23:45:05 +0000

    Task 7 with Constraints

--- Tasks/Task7/task7.js
+++ Tasks/Task7/task7.js
@@ -1,9 +1,9 @@
 function calculateArea() {
-  console.log(4);
   var length = document.getElementById("length").value;
   var width = document.getElementById("width").value;
   var areaFeet = width * length;
-  console.log(areaFeet);
+  var metreConversionFactor = 0.09290304;
+  var areaMetres = areaFeet * metreConversionFactor;
   document.getElementById("areaFeet").innerHTML = areaFeet + " feet squared";
-  document.getElementById("areaMetres").innerHTML = (areaFeet * 0.09290304) + " metres squared";
+  document.getElementById("areaMetres").innerHTML = areaMetres + " metres squared";
 }
