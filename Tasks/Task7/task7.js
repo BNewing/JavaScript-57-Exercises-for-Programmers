9e3516114108dc65c18cfcd281db937c327ccf17
BNewing <bnewing92@hotmail.co.uk>
2016-11-29 00:11:39 +0000

    Addition of Challenge 2 for Task 7

--- Tasks/Task7/task7.js
+++ Tasks/Task7/task7.js
@@ -2,7 +2,7 @@ function checkIfNumber() {
   var length = document.getElementById("length").value;
   var width = document.getElementById("width").value;
   if (isNaN(length) != true && isNaN(width) != true){
-    calculateArea(width, length);
+    checkInputType(width, length);
   }
   else {
     document.getElementById("areaFeet").innerHTML = "You need to add in numbers!"
@@ -10,10 +10,34 @@ function checkIfNumber() {
   }
 }
 
-function calculateArea(width, length){
+function checkInputType(width, length) {
+  var metreRadioButton = document.getElementById("metres").checked;
+  var feetRadioButton = document.getElementById("feet").checked;
+  if (metreRadioButton === false && feetRadioButton === true){
+    calculateAreaWithFeet(width,length)
+  }
+  else if (metreRadioButton === true && feetRadioButton === false ) {
+    calculateAreaWithMetres(width, length)
+  }
+  else{
+    document.getElementById("areaFeet").innerHTML = "You need to say if your input is in metres or feet!"
+    document.getElementById("areaMetres").innerHTML = "";
+  }
+}
+
+function calculateAreaWithFeet(width, length){
+  var inputType = document.getElementById
   var areaFeet = width * length;
   var metreConversionFactor = 0.09290304;
   var areaMetres = areaFeet * metreConversionFactor;
+  document.getElementById("areaFeet").innerHTML = areaFeet + " feet squared";
+  document.getElementById("areaMetres").innerHTML = areaMetres + " metres squared";
+}
+
+function calculateAreaWithMetres(width, length){
+  var areaMetres = width * length;
+  var feetConversionFactor = 10.7639;
+  var areaFeet = areaMetres * feetConversionFactor;
   document.getElementById("areaFeet").innerHTML = areaFeet + " feet squared";
   document.getElementById("areaMetres").innerHTML = areaMetres + " metres squared";
 }
