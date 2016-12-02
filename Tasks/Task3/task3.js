05a642ae333cd140c009d2b43dadcc5d3180ec04
BNewing <bnewing92@hotmail.co.uk>
2016-12-02 20:49:31 +0000

    Tidy up of Task 3

--- Tasks/Task3/task3.js
+++ Tasks/Task3/task3.js
@@ -1,11 +1,6 @@
-console.log(3)
-
 function getSentence(){
-  console.log(5);
   var quote = document.getElementById("quote").value;
-  console.log(5);
   var author = document.getElementById("author").value;
-  console.log(author);
-  document.getElementById("sentence").innerHTML = author + " says: \"" + quote + "\"";
+  document.getElementById("sentence").innerHTML = author + " said: \"" + quote + "\"";
   form.reset();
 }
