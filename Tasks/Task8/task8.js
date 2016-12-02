2014de1342a9ccafce5f9f04c149299d634f2845
BNewing <bnewing92@hotmail.co.uk>
2016-12-02 17:43:17 +0000

    Addition of number collection function and html form

---
+++ Tasks/Task8/task8.js
@@ -0,0 +1,11 @@
+function collectNumbers() {
+  var people = document.getElementById("people").value;
+  var pizza = document.getElementById("pizza").value;
+  document.getElementById("pizza-and-people").innerHTML = people + " people with " + pizza + " pizzas.";
+  form.reset();
+  calculateSlices(people, pizza);
+}
+
+function calculateSlices(people, pizza) {
+  
+}
