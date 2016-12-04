6b067d14496862a15c3fbf559775f434d0928a95
BNewing <bnewing92@hotmail.co.uk>
2016-12-04 19:51:39 +0000

    Completed TAsk 9

---
+++ Tasks/Task9/task9.js
@@ -0,0 +1,15 @@
+console.log(2);
+
+function getDimensions() {
+  var width = document.getElementById("width").value;
+  var length = document.getElementById("length").value;
+  var feetSquared = width * length;
+  calculateGallons(feetSquared);
+}
+
+function calculateGallons(feetSquared) {
+  var constant = 360;
+  var gallons = feetSquared/constant;
+  var gallonsRequired = Math.ceil(gallons);
+  document.getElementById("gallonsNeeded").innerHTML = "You'll need " + gallonsRequired + " tins of paint to be able to paint your ceiling."
+}
