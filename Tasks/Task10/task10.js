7d9b07f9619f30408ec354a2d7ec2097246eb53e
BNewing <bnewing92@hotmail.co.uk>
2016-12-05 13:24:37 +0000

    Tidy up of Program 10

--- Tasks/Task10/task10.js
+++ Tasks/Task10/task10.js
@@ -3,13 +3,16 @@ function getAmounts(){
   var item1Quantity = document.getElementById("item1Quantity").value;
   var item2Price = document.getElementById("item2Price").value;
   var item2Quantity = document.getElementById("item2Quantity").value;
-  calculateCosts(item1Price, item1Quantity, item2Price, item2Quantity)
+  var item3Price = document.getElementById("item3Price").value;
+  var item3Quantity = document.getElementById("item3Quantity").value;
+  calculateCosts(item1Price, item1Quantity, item2Price, item2Quantity, item3Price, item3Quantity)
 }
 
-function calculateCosts(item1Price, item1Quantity, item2Price, item2Quantity){
+function calculateCosts(item1Price, item1Quantity, item2Price, item2Quantity, item3Price, item3Quantity){
   var item1TotalPrice = item1Price * item1Quantity;
   var item2TotalPrice = item2Price * item2Quantity;
-  var subtotal = item1TotalPrice + item2TotalPrice;
+  var item3TotalPrice = item3Price * item3Quantity;
+  var subtotal = item1TotalPrice + item2TotalPrice + item3TotalPrice;
   var taxRate = 0.055;
   var tax = subtotal * taxRate;
   var total = subtotal + tax;
