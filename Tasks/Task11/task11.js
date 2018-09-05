a4f4f1ff2a53a6d0bf0d2fa75d23c25f4b84f0cc
BNewing <bnewing92@hotmail.co.uk>
2018-09-06 00:04:42 +0100

    Adds labels to all form fields

--- Tasks/Task11/task11.js
+++ Tasks/Task11/task11.js
@@ -1,5 +1,25 @@
 function calculateAmount(){
-  var euros = document.getElementById("euros").value;
-  var exchangeRate = document.getElementById("exchangeRate").value;
+	let euros = document.getElementById("euros").value;
+	let exchangeRate = document.getElementById("exchangeRate").value;
+	let result = euros * exchangeRate;
+	let resultRounded = result.toFixed(2);
+	let resultText = euros + " euros at an exchange rate of " + exchangeRate + " is " + resultRounded + " U.S. dollars.";
+	document.getElementById("amount").innerHTML = resultText;
+}
 
+function checkFilledOut(){
+	let euros = document.getElementById("euros");
+	let exchangeRate = document.getElementById("exchangeRate");
+	if(!euros.value) {
+		euros.style.borderColor = "red";
+	}
+	else if (!exchangeRate.value) {
+		exchangeRate.style.borderColor = "red";
+		euros.style.borderColor = "white";
+	}
+	else {
+		euros.style.borderColor = "white";
+		exchangeRate.style.borderColor = "white";
+		calculateAmount()
+	}
 }
