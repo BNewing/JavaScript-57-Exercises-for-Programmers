c6a7a61f07eefde20fd23798d55c7f8af565a543
BNewing <bnewing92@hotmail.co.uk>
2018-11-23 16:58:06 +0000

    First working test

--- Tasks/Task5/task5.js
+++ Tasks/Task5/task5.js
@@ -1,23 +1,52 @@
-function maths() {
+var addition, subtraction, division, multiplication;
+
+function collectValues() {
   var firstNumberValue = document.getElementById("first").value;
   var secondNumberValue = document.getElementById("second").value;
   var firstNumber = parseInt(firstNumberValue);
   var secondNumber = parseInt(secondNumberValue);
   if (isNaN(firstNumber) === false && isNaN(secondNumber) === false){
-    print(firstNumber, secondNumber);
+    doMaths(firstNumber, secondNumber);
  }
  else {
    alert('You need to enter numbers!');
  }
 }
 
-function print(firstNumber, secondNumber){
-  var addition = firstNumber + secondNumber;
-  var subtraction = firstNumber - secondNumber;
-  var division = firstNumber/secondNumber;
-  var multiplication = firstNumber * secondNumber;
+function doMaths(firstNumber, secondNumber){
+  addValues(firstNumber, secondNumber);
+  subtractValues(firstNumber, secondNumber);
+  divideValues(firstNumber, secondNumber);
+  multiplyValues(firstNumber, secondNumber);
+  displayOnScreen(firstNumber, secondNumber);
+}
+
+function addValues(firstNumber, secondNumber){
+  addition = firstNumber + secondNumber;
+  return addition;
+}
+
+function subtractValues(firstNumber, secondNumber){
+  subtraction = firstNumber - secondNumber;
+  return subtraction;
+}
+
+function divideValues(firstNumber, secondNumber){
+  division = firstNumber / secondNumber;
+  return division;
+}
+
+function multiplyValues(firstNumber, secondNumber){
+  multiplication = firstNumber * secondNumber;
+  return multiplication;
+}
+
+
+function displayOnScreen(firstNumber, secondNumber) {
   document.getElementById("add").innerHTML = firstNumber + " + " + secondNumber +  " = " + addition;
   document.getElementById("subtract").innerHTML = firstNumber + " - " + secondNumber +  " = " + subtraction;
   document.getElementById("divide").innerHTML = firstNumber + " / " + secondNumber +  " = " + division;
   document.getElementById("multiply").innerHTML = firstNumber + " x " + secondNumber +  " = " + multiplication;
 }
+
+module.exports = addValues;
